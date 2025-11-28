66c707a91025bdaae08570d6d71f60daf60b7684
marlevek <marcelo_levek@hotmail.com>
2025-11-28 15:44:13 -0300

    add whitenoise and correct settings.py

--- app_core/settings.py
+++ app_core/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app_core.urls'
@@ -161,3 +162,6 @@ LOGGING = {
         },
     },
 }
+
+# Configuração WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
