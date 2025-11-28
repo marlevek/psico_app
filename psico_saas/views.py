db07fc83e56714e882e4a790c5ec77fc62a4581a
marlevek <marcelo_levek@hotmail.com>
2025-11-28 14:29:09 -0300

    fixed class PlanoTratamentoViewSet em views.py

--- psico_saas/views.py
+++ psico_saas/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets 
-from .models import PlanoTratamento 
+from .models import PlanoTratamento # <-- AQUI ESTÁ O PRÓXIMO PONTO DE FALHA
 from .serializers import PlanoTratamentoSerializer 
-
 
 class PlanoTratamentoViewSet(viewsets.ModelViewSet):
     '''
     Endpoint da API que permite criar, visualizar, atualizar e deleter planos de tratamento
     '''
-    # REMOVIDA: queryset = PlanoTratamento.objects.all().order_by('-data_criacao')
-    
     # CORREÇÃO: Usar get_queryset() para ADIAR a query do banco de dados 
-    # até que o servidor esteja completamente ativo e o Gunicorn esteja pronto.
     def get_queryset(self):
+        # Esta função só executa quando o servidor já está a funcionar
         return PlanoTratamento.objects.all().order_by('-data_criacao')
         
     serializer_class = PlanoTratamentoSerializer
-
